a72bfbfeb6a28e285290a9a63d2ac75f028d8ff8
Johann Klemmack <johann.klemmack@gmail.com>
2018-09-06 18:52:13 -0500

    Bare ServiceStack ASP.Net Core with Razor

---
+++ SSNetCoreWiki.ServiceInterface/MyServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ServiceStack;
+using ServiceStack.Templates;
+using ServiceStack.DataAnnotations;
+using SSNetCoreWiki.ServiceModel;
+
+namespace SSNetCoreWiki.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
